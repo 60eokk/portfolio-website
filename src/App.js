04a6a19dae73acef2501275e3ad3b1f691797f8d
Keith Jung <60eokk@gmail.com>
2024-08-11 17:28:47 -0500

    app.js making projects list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [scrollY, setScrollY] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  const 
+
+  const projects = [
+    { id: 1, name: 'Project 1', description: 'Description of Project 1' },
+    { id: 2, name: 'Project 2', description: 'Description of Project 2' },
+    { id: 3, name: 'Project 3', description: 'Description of Project 3' },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='Welcome to my Portfolio Page'></h1>
       </header>
+
+      <section className='about-me'>
+        <h2 About Me></h2>
+        <p className='I am a SWE XXXXXXXX'></p>
+      </section>
+
+      <section className='projects'>
+        <h2 Projects></h2>
+        <div className='projects-list'>
+          <h3>{projects[project1].name}</h3>
+          <p>{projects[project1].description}</p>
+          <button onClick={nextProject}>Next Project</button>
+        </div>
+      </section>
     </div>
   );
 }
+
 
 export default App;
